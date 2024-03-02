74fc6136304106a68681e666a6e206a581a9926a
Andre Ackermann <andre.a@gmx.de>
2024-03-02 20:53:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# ESP32 Bluetooth Classic / BLE keyboard to USB over I2C adapter
+
+Project to connect a Bluetooth Classic or BLE keyboard wirelessly via USB to a computer. Usefull for editing BIOS-Settings or unlocking full-disk-encryption when bluetooth is not (yet) available.
+
+If your keyboard only supports Bluetooth Classic, you need this code on a ESP32 (plain), which to my knowledge is the only one supporting Bluetooth Classic and a second ESP32 with USB-OTG Support (eg ESP32-S3).
+
+The HID-Reports received via bluetooth on the first ESP32 (plain) are send over I2C to the second ESP32 (S3) that acts as the USB-Connector, receives the HID-Reports over I2C and resends them over USB to the connected host-PC. 
+The software for the I2C-receiver is here: https://github.com/acepe/esp32-I2C-receiver
+
+ **Developed and tested on the ESP32-WROOM-32 DevKit board, other variants may not work!**
+
+WARNING: This project is for use in ESP-32 (plain) module with Bluetooth Classic and BLE support. If you have another variant, you'll have to adapt the code. Newer ESP32 (C, S, ...) only support Bluetooth LE.
+
+# Missing Features
+
+Keyboard LEDs (Caps-Lock, Num-Lock etc) are not working (the keys are working fine, just not the LEDs).
+Media-Keys might not work.
+
+I currently have no intention to fix that, because it is not needed for my usecase.
+
+# Building and flashing
+
+Project works as-is under Visual Studio Code (2023). You need to have the Espressif IDF extension installed and the v5.1 of the ESP-IDF SDK. Workflow is as follows:
+
+1- Install Visual Studio Code
+
+2- On the left, open the extensions panel
+
+3- Search and install the Espressif IDF extension
+
+4- Open the command prompt pressing Ctrl+Shift+P
+
+5- Execute command "ESP-IDF: Configure ESP-IDF extension"
+
+6- Select Express, and under "Select ESP-IDF version" choose v5.1-rc2
+
+7- After installation, select File > Open Folder and open the ESP32-BT2I2C project folder
+
+8- Start building by pressing Ctrl+E and then B, or using the Build button in the bottom bar
+
+9- If succesfully built, connect and flash your ESP32 board (Ctrl+E then F, or the flash button)
+
+Refer to the following link for more instructions:
+
+* https://github.com/espressif/vscode-esp-idf-extension/blob/master/docs/tutorial/install.md
+
+Once succesfully built and flashed, you're ready to rock!.
+
+
+# Usage and debugging
+
+Once powered up the module scans for nearby BLE devices. If the last bonded keyboard is in range, it will try to connect to it using the keys stored on the NVS flash, so no pairing is needed for every connection. If it doesn't detect a previously bonded device, it will try to connect to the nearest keyboard in pairing mode. If both processes fail, it will wait one second and scan again until it finds anything.
+
+BLE KEYBOARD PAIRING: Set keyboard in pairing mode and power on the board. No code entry required.
+
+BLUETOOTH CLASSIC KEYBOARD PAIRING (code pairing):
+
+1- Set keyboard in pairing mode and power on the board
+
+2- Wait until you see a short blast of quick flashes, then pay attention:
+
+3- For each code digit, the board will flash the LED the number of times equal to the digit
+
+4- Press each digit on the keyboard as you read them (do not wait until it finishes! Some keyboards have a timeout)
+
+5- If the digit is 0, a steady light will show for 1.5 seconds instead of the digit
+
+6- When you see the blast of quick flashes again, press ENTER
+
+OR you can always look at the code using the Serial monitor console, whatever you find easier, you fancy boy.
+
+Once connected you can start using your keyboard, blue LED should be on. Connect the board to the computer or PS/2 compatible system and enjoy.
+
+You can hot-disconnect the keyboard. The module will detect the disconnection and repeatedly try to reconnect, so it will be back online as soon as the keyboard gets up again. This is critical for keyboards that go to sleep and disconnect, or if you swap between computers using a multi-connection keyboard. 
+
+Please note that pairing is only done after power up. If you wish to pair a new device, please reset the module with the reset button or power off and on the computer (not just reset it, because we need a power cycle). Note that if a previously paired device is on and in range, it will always connect to it first.
+
+In case something doesn't work, you'll need to debug. 
+
+# History
+ 
+ * based heavily on esp32-BT2PS2 by Hambert, 2023. https://github.com/Hamberthm/esp32-bt2ps2
+ * adapted to use a I2C-Connection instead of PS2 by acepe, 2024
+ 
---
+++ main/main.cpp
@@ -0,0 +1,287 @@
+/*
+ESP32-BT2USB
+Software for Espressif ESP32 (plain) for Bluetooth Classic and BLE HID keyboard to USB interfacing over I2C.
+A second ESP32 with USB-OTG support is needed, running this software: https://github.com/acepe/esp32-I2C-receiver 
+Based on esp32-BT2PS2 (https://github.com/Hamberthm/esp32-bt2ps2) by Humberto Möckel. Adapted by André Ackermann for I2C interface instead of PS2.
+Thanks to Humberto Möckel and all the pioneers who worked on the code libraries that made this project possible, check them on the README!
+Copyright Humberto Möckel - Hamcode - 2023 (hamberthm@gmail.com), André Ackermann - 2024,
+*/
+
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "string.h"
+
+#include "../include/globals.hpp"
+#include "../include/bt_keyboard.hpp"
+
+static constexpr char const *TAG = "BTKeyboard";
+
+#define I2C_MASTER_SCL_IO 22      /*!< GPIO number for I2C master clock */
+#define I2C_MASTER_SDA_IO 23      /*!< GPIO number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_0  /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000 /*!< I2C master clock frequency */
+#define I2C_SLAVE_ADDR 0x27       /*!< I2C slave address for the slave device */
+
+// BTKeyboard section
+BTKeyboard bt_keyboard;
+
+int NumDigits(int x) // Returns number of digits in keyboard code
+{
+    x = abs(x);
+    return (x < 10 ? 1 : (x < 100 ? 2 : (x < 1000 ? 3 : (x < 10000 ? 4 : (x < 100000 ? 5 : (x < 1000000 ? 6 : (x < 10000000 ? 7 : (x < 100000000 ? 8 : (x < 1000000000 ? 9 : 10)))))))));
+}
+
+void pairing_handler(uint32_t pid)
+{
+    int x = (int)pid;
+    std::cout << "Please enter the following pairing code, "
+              << std::endl
+              << "followed by ENTER on your keyboard: "
+              << pid
+              << std::endl;
+
+    for (int i = 0; i < 10; i++) // Flash quickly many times to alert user of incoming code display
+    {
+        gpio_set_level(GPIO_NUM_2, 1);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_2, 0);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+
+    int dig = NumDigits(x); // How many digits does our code have?
+
+    for (int i = 1; i <= dig; i++)
+    {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        ESP_LOGW(TAG, "PAIRING CODE (TYPE ON KEYBOARD AND PRESS ENTER): %d ", pid);
+        int flash = ((int)((pid / pow(10, (dig - i)))) % 10); // This extracts one ditit at a time from our code
+        ESP_LOGI(TAG, "Flashing %d times", flash);
+        for (int n = 0; n < flash; n++) // Flash the LED as many times as the digit
+        {
+            gpio_set_level(GPIO_NUM_2, 1);
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+            gpio_set_level(GPIO_NUM_2, 0);
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+        }
+
+        if (flash < 1) // If digit is 0, keep a steady light for 1.5sec
+        {
+            gpio_set_level(GPIO_NUM_2, 1);
+            vTaskDelay(1500 / portTICK_PERIOD_MS);
+            gpio_set_level(GPIO_NUM_2, 0);
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+        }
+    }
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    for (int i = 0; i < 10; i++) // Quick flashes indicate end of code display
+    {
+
+        gpio_set_level(GPIO_NUM_2, 1);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_2, 0);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+}
+
+extern "C"
+{
+
+// HID report structure for keyboards
+    typedef struct 
+    {
+        uint8_t modifiers;
+        uint8_t reserved;
+        uint8_t keys[6];
+    } hid_keyboard_report_t;
+  
+  
+    void i2c_master_init() 
+    {
+        i2c_config_t conf;
+        conf.mode = I2C_MODE_MASTER;
+        conf.sda_io_num = I2C_MASTER_SDA_IO;
+        conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+        conf.scl_io_num = I2C_MASTER_SCL_IO;
+        conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+        conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+        conf.clk_flags = 0;
+        i2c_param_config(I2C_MASTER_NUM, &conf);
+        i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
+
+        ESP_LOGI(TAG, "I2C Initialized");
+    }
+
+    void i2c_master_send_text(const hid_keyboard_report_t *report) 
+    {
+        size_t size = sizeof(hid_keyboard_report_t);
+
+        ESP_LOGD(TAG, "Sending report");
+        ESP_LOG_BUFFER_HEX_LEVEL(TAG, report, size, ESP_LOG_DEBUG);
+
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (I2C_SLAVE_ADDR << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write(cmd, (uint8_t *)report, size, true);
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+    }
+
+    void reset_report(hid_keyboard_report_t *report) 
+    {
+        report->modifiers = 0;  // Reset modifiers
+        for (int i = 0; i < sizeof(report->keys); i++) {
+            report->keys[i] = 0;
+        }
+    }
+   
+    void app_main(void)
+    {
+        gpio_reset_pin(GPIO_NUM_2);                       // using built-in LED for notifications
+        gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); // Set the GPIO as a push/pull output
+        gpio_set_level(GPIO_NUM_2, 1);
+        
+        i2c_master_init();
+        gpio_set_level(GPIO_NUM_2, 0);
+
+        // init BTKeyboard
+        esp_err_t ret;
+
+        // To test the Pairing code entry, uncomment the following line as pairing info is
+        // kept in the nvs. Pairing will then be required on every boot.
+        // ESP_ERROR_CHECK(nvs_flash_erase());
+
+        ret = nvs_flash_init();
+        if ((ret == ESP_ERR_NVS_NO_FREE_PAGES) || (ret == ESP_ERR_NVS_NEW_VERSION_FOUND))
+        {
+            ESP_ERROR_CHECK(nvs_flash_erase());
+            ret = nvs_flash_init();
+        }
+        ESP_ERROR_CHECK(ret);
+
+        if (bt_keyboard.setup(pairing_handler))
+        { // Must be called once
+            while (!bt_keyboard.devices_scan())
+            {
+                if (BTKeyboard::btFound)
+                {
+                    BTKeyboard::btFound = false;
+                    for (int i = 0; i < 60; i++)
+                    {
+                        if (BTKeyboard::isConnected)
+                            break;
+                        ESP_LOGI(TAG, "Waiting for BT manual code entry...");
+                        vTaskDelay(1000 / portTICK_PERIOD_MS);
+                    }
+                }
+                else
+                {
+                    vTaskDelay(1000 / portTICK_PERIOD_MS);
+                    ESP_LOGI(TAG, "Rescan...");
+                }
+            }                              // Required to discover new keyboards and for pairing
+                                           // Default duration is 5 seconds
+            gpio_set_level(GPIO_NUM_2, 1); // success, device found
+        }
+
+        uint8_t typematicRate = 20;    // characters per second in Typematic mode
+
+        // fixed stuff
+        uint8_t cycle = 1000 / typematicRate;            // keywait timeout in ms. Important so we can check connection and do Typematic
+        TickType_t repeat_period = pdMS_TO_TICKS(cycle); // keywait timeout in ticks. Important so we can check connection and do Typematic
+        BTKeyboard::KeyInfo info;                        // freshly received
+        BTKeyboard::KeyInfo infoBuf;                     // currently pressed
+        bool found = false;                              // just an innocent flasg I mean flag
+
+        info.modifier = infoBuf.modifier = (BTKeyboard::KeyModifier)0;
+
+        for (int j = 0; j < BTKeyboard::MAX_KEY_COUNT; j++)
+        {
+            infoBuf.keys[j] = 0;
+            info.keys[j] = 0;
+        }
+
+        while (true)
+        {
+            if (bt_keyboard.wait_for_low_event(info, repeat_period))
+            {
+                hid_keyboard_report_t report;
+                reset_report(&report);
+
+                report.modifiers = (uint8_t)info.modifier;
+
+                // release the keys that have been just released
+                for (int i = 0; i < BTKeyboard::MAX_KEY_COUNT; i++)
+                {
+                    if (!infoBuf.keys[i]) // Detect END FLAG
+                        break;
+                    for (int j = 0; j < BTKeyboard::MAX_KEY_COUNT; j++)
+                    {
+                        if (infoBuf.keys[i] == info.keys[j])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        ESP_LOGD(TAG, "Up key: %x", infoBuf.keys[i]);
+                        gpio_set_level(GPIO_NUM_2, 1);
+
+                        report.keys[i] = info.keys[i];
+                    }
+                    else
+                        found = false;
+                }
+
+                // press the keys that have been just pressed
+                for (int i = 0; i < BTKeyboard::MAX_KEY_COUNT; i++)
+                {
+                    if (!info.keys[i]) // Detect END FLAG
+                        break;
+                    for (int j = 0; (j < BTKeyboard::MAX_KEY_COUNT); j++)
+                    {
+                        if (info.keys[i] == infoBuf.keys[j])
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        ESP_LOGD(TAG, "Down key: %x", info.keys[i]);
+                        gpio_set_level(GPIO_NUM_2, 0);
+
+                        report.keys[i] = info.keys[i];
+                    }
+                    else
+                        found = false;
+                }
+                
+                i2c_master_send_text(&report);
+
+                infoBuf = info;                 // Now all the keys are handled, we save the state
+            }
+
+            else
+            {
+                while (!BTKeyboard::isConnected)
+                {                                  // check connection
+                    gpio_set_level(GPIO_NUM_2, 0); // disconnected
+                    bt_keyboard.quick_reconnect(); // try to reconnect
+                    vTaskDelay(250 / portTICK_PERIOD_MS);
+                }
+                gpio_set_level(GPIO_NUM_2, 1);
+            }
+        }
+    }
+}
